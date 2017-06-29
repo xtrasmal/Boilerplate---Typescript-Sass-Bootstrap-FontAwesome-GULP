ea6b925a987d1c4df02e9d9dedbce7db05eb064c
MikeOne <michiel@kikkert.nl>
2017-06-29 15:01:20 +0200

    Included Tether.js
    Updated tether to be included (needed for Bootstrap4)

--- src/vendor/vendor.js
+++ src/vendor/vendor.js
@@ -1,3 +1,4 @@
 //= require ../../node_modules/jquery/dist/jquery.js
+//= require ../../node_modules/tether/dist/js/tether.js
 //= require ../../node_modules/bootstrap/dist/js/bootstrap.js
 //= require ../../node_modules/systemjs/dist/system.js
